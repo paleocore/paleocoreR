ea91eb1bddcd2e091fbeea09a276ed5c3119e9ee
wabarr <wabarr@gmail.com>
2014-05-30 10:31:42 -0500

    check for username and api_key

--- R/getPCdata.R
+++ R/getPCdata.R
@@ -13,6 +13,7 @@
 
 getPCdata <- function(project="turkana", version="v1", base_url="http://paleocore.org/API",format="csv", ...) {
   require(httr)
+  #format the filters as GET parameters
   filter <- paste(
                 paste(
                   names(list(...)),
@@ -36,7 +37,13 @@ getPCdata <- function(project="turkana", version="v1", base_url="http://paleocor
                     )
   
   attempt <-  GET(url = formattedURL)
-    if (attempt$status_code == 401) stop ("You are not authorized to get this data. Have you saved your username and api_key using setPCcredentials()")
+  if (attempt$status_code == 401) {
+    #if unauthorized, try it with username and api_key
+    if(any(!exists('username'), !exists('api_key'))) stop("You need to set your username and api_key using setPCcredentials()")
+    attempt <-  GET(url = sprintf("%s&username=%s&api_key=%s", formattedURL, username, api_key))
+  }
+  
+    if (attempt$status_code == 401) stop (sprintf("User %s with api_key=%s is not authorized to get this data.", username, api_key))
     if (attempt$status_code != 200) stop ("There was an error.  Maybe you mispelled something or tried to filter on a field that doesn't exist?")
   return(read.table(text = content(attempt), header=TRUE, sep=","))
 }
