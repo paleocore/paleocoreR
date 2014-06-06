abbd711c24fbedd6db4dacb42a30e6b06433975e
wabarr <wabarr@gmail.com>
2014-06-06 10:11:25 -0500

    simplified base_url

--- R/getFilters.R
+++ R/getFilters.R
@@ -3,20 +3,20 @@
 #' This function gets a listing of the fields that are available for filtering when using the getPCdata() function
 #' @param project The name of the paleocore project to get information on
 #' @param version The version of the API to use, defaults to "v1"
-#' @param base_url The base url for the API, with no trailing slash. Defaults to http://paleocore.org/API
+#' @param base_url The base url for the API, with no trailing slash. Defaults to http://paleocore.org/
 #' @keywords PaleoCore API paleoanthropology
 #' @export
 #' @examples
 #' getFilters("turkana")
 
-getFilters <- function(project, version="v1", base_url="http://paleocore.org/API") {
+getFilters <- function(project, version="v1", base_url="http://paleocore.org") {
   require(httr)
-  requestURL <- paste(paste(base_url, version, project, "schema/", sep="/"),"format=json", sep="?")
+  requestURL <- paste(paste(base_url, "API", version, project, "schema/", sep="/"),"format=json", sep="?")
   attempt <- GET(requestURL)
   
   if (attempt$status_code == 401) {
     #if unauthorized, try it with username and api_key
-    if(any(!exists('username'), !exists('api_key'))) stop("You need to set your username and api_key using setPCcredentials()")
+    if(any(!exists('username'), !exists('api_key'))) stop("You need to set your username and api_key using setPCcredentials(). You can get an apikey at http://paleocore.org/apikey")
     attempt <-  GET(url = sprintf("%s&username=%s&api_key=%s", requestURL, username, api_key))
   }
   
@@ -28,7 +28,7 @@ getFilters <- function(project, version="v1", base_url="http://paleocore.org/API
   message(sprintf("The following fields from the %s project can be passed as query filters using getPCdata().", project))
   print(names(schema$filtering))
   message("Example usage:")
-  examples<-paste0(names(schema$filtering)[1], c("", "__exact","__contains", "__startswith", "__endswith"), "=", "'somevalue'")
+  examples<-paste0(names(schema$filtering)[1], c("", "__exact","__contains", "__lt","__gt", "__startswith", "__endswith"), "=", "'somevalue'")
   invisible(lapply(examples, FUN=message))#invisible() stops lapply from printing the list being iterated over to the console
 
 }
--- R/getPCdata.R
+++ R/getPCdata.R
@@ -3,7 +3,7 @@
 #' This function creates a properly formatted URL string and attempts to get the corresponding data from the Paleocore API
 #' @param project Name of paleocore project you want data from. Default is "turkana"
 #' @param version Which version of the paleocore to use. Default is "v1"
-#' @param base_url The base url for the API, with no trailing slash. Defaults to http://paleocore.org/API
+#' @param base_url The base url for the API, with no trailing slash. Defaults to http://paleocore.org
 #' @param limit Limits the number of records returned.  Default is 20. Use `limit=0` to return all matching records. 
 #' @param ... An arbitrary number of filter critera in the following form: genus__contains="Austral"
 #' @keywords PaleoCore API paleoanthropology
@@ -11,7 +11,7 @@
 #' @examples
 #' getPCdata(tribe__contains="Tragel", limit=0)
 
-getPCdata <- function(project="turkana", version="v1", base_url="http://paleocore.org/API", ...) {
+getPCdata <- function(project="turkana", version="v1", base_url="http://paleocore.org", ...) {
   require(httr)
   #format the filters as GET parameters
   filter <- paste(
@@ -26,7 +26,7 @@ getPCdata <- function(project="turkana", version="v1", base_url="http://paleocor
   URL_parameters = paste0("?format=csv", "&", filter)
   
   formattedURL <- paste(
-                      paste(base_url, version, project, sep="/"), 
+                      paste(base_url, "API", version, project, sep="/"), 
                       URL_parameters, 
                       sep="/"
                       )
