637bd3e5a3ae92d655493243dd99f4d4fca09853
wabarr <wabarr@gmail.com>
2014-06-11 09:36:36 -0500

    add argument to list_pcore_datasets to optionally return list of datasets, rather than merely printing them

--- R/list_pcore_datasets.R
+++ R/list_pcore_datasets.R
@@ -3,12 +3,13 @@
 #' This function gets a listing of the individual data datasets that are accessible using the get_pcore_data() function
 #' @param version The version of the API to use, defaults to "v1"
 #' @param base_url The base url for the API, with no trailing slash. Defaults to http://paleocore.org
+#' @param return_list Boolean. Whether or not to return a list of available datasets, or if not, to simply print them to console.  Default is FALSE.
 #' @keywords PaleoCore API paleoanthropology
 #' @export
 #' @examples
 #' list_pcore_datasets("turkana")
 
-list_pcore_datasets <- function(version="v1", base_url="http://paleocore.org") {
+list_pcore_datasets <- function(version="v1", base_url="http://paleocore.org", return_list=FALSE) {
   require(httr)
   requestURL <- paste(paste(base_url, "API", version, sep="/"),"format=json", sep="?")
   attempt <- GET(requestURL)
@@ -33,4 +34,5 @@ list_pcore_datasets <- function(version="v1", base_url="http://paleocore.org") {
     if(!is.null(datasets[[datasetName]])) print(paste(datasetName, datasets[[datasetName]], sep=": "), quote=FALSE)
     })
   )
+  if(return_list) return(datasets)
  }
